0a91f7afdef84e5701abc42fe23ec03440acb9ab
Rainshn <rainshn1@gmail.com>
2025-11-16 21:36:54 +0900

    기본 패키지 생성

---
+++ src/main/java/com/swulion/puppettale/PuppetTaleApplication.java
@@ -0,0 +1,13 @@
+package com.swulion.puppettale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppetTaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppetTaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swulion/puppettale/PuppetTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swulion.puppettale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppetTaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
